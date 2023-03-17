0a642a5cd9f89fe8dfc92311c0c6ad970a82451f
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-03-18 02:30:47 +0300

    Внесены правки в модльное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,15 +1,31 @@
 const modal = () => {
   const modalOverlay = document.querySelector(".modal-overlay");
   const modalContent = document.querySelector(".modal-callback");
+  const fixBlock = document.querySelectorAll(".fix-block");
   const modal = [modalOverlay, modalContent];
+
+  let paddingOffset = window.innerWidth - document.body.offsetWidth + "px";
+
+  const disableScroll = () => {
+    fixBlock.forEach((item) => (item.style.paddingRight = paddingOffset));
+    document.body.style.paddingRight = paddingOffset;
+  };
+
+  const enableScroll = () => {
+    fixBlock.forEach((item) => (item.style.paddingRight = ""));
+    document.body.style.paddingRight = "";
+  };
+
   const openModal = () => {
     modal.forEach((item) => {
       if (!item.style.display) {
         item.style.display = "block";
         document.body.style.overflow = "hidden";
+        disableScroll();
       } else {
         item.style.display = "";
         document.body.style.overflow = "";
+        enableScroll();
       }
     });
   };
